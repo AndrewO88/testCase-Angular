21f5bce5dbaf23d7a6c5cb0cdb9b5d4a6c661036
youngSenior <youngsenior88@icloud.com>
2022-03-09 18:31:51 +0300

    complete

--- src/app/posts.ts
+++ src/app/posts.ts
@@ -7,23 +7,22 @@ export interface Post {
 export const POSTS: Post[] = [
   {
     title: 'Заметка 1',
-    // tslint:disable-next-line:max-line-length
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. \n Commodi doloremque eligendi ipsam iure iusto minima reiciendis tempora. \n  Autem blanditiis consectetur debitis dicta illum perferendis quisquam sunt, ullam voluptatem? Eius expedita fugit libero modi necessitatibus odio odit, officiis saepe sapiente veritatis?',
     id: 1
   },
   {
     title: 'Заметка 2',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. \n  Commodi doloremque eligendi ipsam iure iusto minima reiciendis tempora. \n  Autem blanditiis consectetur debitis dicta illum perferendis quisquam sunt, ullam voluptatem? Eius expedita fugit libero modi necessitatibus odio odit, officiis saepe sapiente veritatis?',
+    text:' Lorem ipsum dolor sit amet, consectetur adipisicing elit. \n Animi consectetur debitis deserunt dolores doloribus eos facilis fugit harum in, inventoreipsum iste iusto laborum magnam maxime molestiae mollitia necessitatibus nobis provident quod quosreiciendis repellat reprehenderit, rerum similique ut vero.',
     id: 2
   },
   {
     title: 'Заметка 3',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. \n  Commodi doloremque eligendi ipsam iure iusto minima reiciendis tempora. \n  Autem blanditiis consectetur debitis dicta illum perferendis quisquam sunt, ullam voluptatem? Eius expedita fugit libero modi necessitatibus odio odit, officiis saepe sapiente veritatis?',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. \n  A ad amet architecto asperiores at, debitis dolorum eveniet fugiat ipsum laborum libero magni maxime minima, necessitatibus neque nesciunt nostrum pariatur praesentium quisquam quod recusandae rerum sapiente tempore ullam voluptates. \n  Commodi cupiditate delectus esse id incidunt maiores odio odit repellat reprehenderit voluptatibus?',
     id: 3
   },
   {
     title: 'Заметка 4',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.  \n  Commodi doloremque eligendi ipsam iure iusto minima reiciendis tempora. \n  Autem blanditiis consectetur debitis dicta illum perferendis quisquam sunt, ullam voluptatem? Eius expedita fugit libero modi necessitatibus odio odit, officiis saepe sapiente veritatis?',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.\n  Ad, commodi cum dolor dolorum earum ipsum, labore magni minus quibusdam quidem quo, reiciendis tempora velit! \n Cum cupiditate delectus ea enim facilis nulla numquam suscipit? \n Accusantium autem beatae culpa ea enim ex exercitationem illo laudantium minus, neque nulla officia possimus praesentium quis, quisquam reprehenderit.',
     id: 4
   },
 ];
